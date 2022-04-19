d0401013b197aae0f476541ce0146d1e473fd4b2
Nicolas Garcia <ni.car.garcia@gmail.com>
2022-04-18 21:38:20 -0300

    style(container): styled scrollbar

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import {
 	background,
 	border,
-	compose,
 	color,
 	flexbox,
 	layout,
@@ -22,6 +21,29 @@ type CustomViewProps = BackgroundProps &
 	LayoutProps &
 	SpaceProps;
 
-export default styled.div<CustomViewProps>(
-	compose(background, border, color, flexbox, layout, space)
-);
+export default styled.div<CustomViewProps>`
+	${background} ${border} ${color} ${flexbox} ${layout} ${space}
+
+	::-webkit-scrollbar {
+		width: 10px;
+		height: 10px;
+	}
+
+	::-webkit-scrollbar-track {
+		box-shadow: inset 0 0 5px grey;
+		border-radius: 10px;
+	}
+
+	::-webkit-scrollbar-thumb {
+		background: #8257e5;
+		border-radius: 10px;
+	}
+
+	::-webkit-scrollbar-thumb:hover {
+		background: #6946b9;
+	}
+
+	::-webkit-scrollbar-corner {
+		display: none;
+	}
+`;
