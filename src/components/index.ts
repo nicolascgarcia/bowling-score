c3add843ec205c2998dbe67cacd670cb561f559f
Nicolas Garcia <ni.car.garcia@gmail.com>
2022-04-18 22:43:16 -0300

    feat(components): importing new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@ import Container from './Container';
 import NameInput from './NameInput';
 import Button from './Button';
 import UserName from './UserName';
+import ScoreColumn from './ScoreColumn';
+import PlayerRounds from './PlayerRounds';
 
-export { Container, NameInput, Button, UserName };
+export { Container, NameInput, Button, UserName, ScoreColumn, PlayerRounds };
